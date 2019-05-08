4babbde2bae120fa9496e93eb6b7889433d8f013
Stanfordna <stanfordnas@gmail.com>
2019-05-07 19:13:08 -0600

    Changed schema agreement for Cleaning Objects to not specify type "date". This because ical object dates seem not to match swagger date format. Fixed issue with attempting to access nonexistent start and end params. getcleanercleanings and getproperty cleanings work with all combinations of parameters.

--- api/controllers/cleanings_controller.js
+++ api/controllers/cleanings_controller.js
@@ -457,7 +457,7 @@ function getpropertycleanings (req, res) {
             }
         });
     } else {
-        Cleaning.find({cleaner: ObjectID(id).toHexString()}, function (err, cleanings) {
+        Cleaning.find({property: ObjectID(id).toHexString()}, function (err, cleanings) {
             if (err) {
                 res.status(404).json({
                     success: false,
